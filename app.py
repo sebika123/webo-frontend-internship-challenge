d05b2d967abd26a34fd3fec2bb6b19d6cacee0be
sebika nepal <72345247+sebika123@users.noreply.github.com>
2023-11-19 22:29:51 +0545

    text area created for input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask,render_template,request
+from flask import Flask,render_template,request
 from text_summary import summarizer
 app=Flask(__name__)
 
@@ -6,7 +6,13 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/analyse', methods=['GET','POST'])
+def analyse():
+    if request.method=='POST':
+       rawtext=request.form['rawtext'] 
+       summary,original_txt,len_orig_txt,len_summary=summarizer(rawtext)
+    return render_template(template_name_or_list)
 
 if __name__=="__main__":
     app.run(debug=True)
+    
