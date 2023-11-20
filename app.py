b5302021a88fe251a114aeb270fa41b53953382e
sebika nepal <72345247+sebika123@users.noreply.github.com>
2023-11-20 21:47:59 +0545

    index and analyze page working

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/analyse', methods=['GET','POST'])
+@app.route('/analyze', methods=['GET','POST'])
 def analyse():
     if request.method=='POST':
        rawtext=request.form['rawtext'] 
        summary,original_txt,len_orig_txt,len_summary=summarizer(rawtext)
-    return render_template(template_name_or_list)
+    return render_template("summary.html",summary=summary,original_txt=original_txt,len_orig_txt=len_orig_txt,len_summary=len_summary)
 
 if __name__=="__main__":
     app.run(debug=True)
